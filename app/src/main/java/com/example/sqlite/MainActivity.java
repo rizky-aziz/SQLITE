dabe513d3564261e9abaea8235c0a99d3df793d7
rizkyaziz2002 <rizkyaziz2002@gmail.com>
2019-09-09 11:51:05 +0700

    Initial commit

---
+++ app/src/main/java/com/example/sqlite/MainActivity.java
@@ -0,0 +1,23 @@
+package com.example.sqlite;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Fragment fragment = new Fragment_Main();
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.add(R.id.framecontainer,fragment);
+        fragmentTransaction.commit();
+    }
+}
